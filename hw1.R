94119b308372c3723a3b3b95ecc527045df3706d
Never Cry <apple@MacBook-Air-Apple-2.local>
2019-10-09 21:22:51 +0300

    First commit

---
+++ hw1.R
@@ -0,0 +1,14 @@
+final_function <- function(data, rowsel, colsel) {
+    newdata <- data[rowsel, colsel] 
+    results <- list()
+    for (i in colnames(newdata)) {
+        if (is.numeric(newdata[,i])) {
+            m <- mean(newdata[, i]) 
+            results[[i]] <- m
+        } else {
+            f <- table(newdata[,i])
+            results[[i]] <- f
+        }
+    }
+    return(list(newdata, results))
+}
